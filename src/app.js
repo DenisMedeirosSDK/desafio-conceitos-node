1f659d72e65e0c9a213ebe7ae982d70ff5ee0cd6
Denis Medeiros <48757658+DenisMedeirosKollosso@users.noreply.github.com>
2020-04-07 20:59:04 -0300

    Routes
    index, store, delete, put

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
+//const axios = require('axios');
 
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +12,62 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+  const result = title
+  ? repositories.filter(repositorie => repositories.title.includes(title))
+  : repositories;
+  
+  return response.json(result);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, tech, urlRepo } = request.body;
+  //const response = await axios.get(`http://api.github.com/repos/DenisMedeirosSDK/${urlRepo}`)
+  
+  const repositorie = {
+    id: uuid(),
+    title,
+    tech,
+    urlRepo
+  };
+
+  repositories.push(repositorie);
+  return response.json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, tech } = request.body;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' })
+  }
+  const repositorie = {
+    id,
+    title,
+    tech
+  }
+  repositories[repositorieIndex] = repositorie;
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return res.status(400).json({ error: 'Repositorie not found' })
+  }
+  repositories.splice(repositorieIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+
 });
 
 module.exports = app;
